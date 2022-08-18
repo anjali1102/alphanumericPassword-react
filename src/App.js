246bc1b1af42914ab32ea95371e2c42b8fd7f140
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-08-18 13:20:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* ex3: 
+Password should contain a number. Show error if user misses character. */
+
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [alphaNum, setAlphaNum] = useState();
+
+  /* optional chaining concept */
+  const inputHandler = (event) => {
+    const value = event?.target?.value || "";
+
+    if (value.match(/[A-Z]/gi) && value.match(/[0-9]/gi)) {
+      setAlphaNum("Success");
+    } else {
+      setAlphaNum("error");
+    }
+  };
+  return (
+    <div className="App">
+      <input
+        style={{ width: "200px", padding: "1rem" }}
+        type="password"
+        placeholder="Enter alphanumeric password"
+        onChange={inputHandler}
+      ></input>
+      <div>{alphaNum}</div>
+    </div>
+  );
+}
